bff27ca0fc5b9e74cbe1afa0a4b65ffbc34464dd
Joe Reeve <joseph.reeve@googlemail.com>
2020-01-22 12:25:36 +0000

    :sparkles: Campus updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { UserList, UserEdit, UserCreate } from './user';
 import simpleRestProvider from 'ra-data-simple-rest'
 import authProvider from './authProvider';
 import LoginPage from './LoginPage';
-import { CampusList } from './campus';
+import { CampusList, CampusCreate, CampusEdit } from './campus';
 import config from './config';
 
 const httpClient = (url, options = {}) => {
@@ -24,7 +24,7 @@ const dataProvider = simpleRestProvider(config.api, httpClient)
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider} loginPage={LoginPage}>
     {user => [
-      <Resource name="campus" list={CampusList} edit={EditGuesser} />,
+      <Resource name="campus" list={CampusList} edit={CampusEdit} create={user.isAdmin ? CampusCreate : null} />,
       <Resource name="user" list={UserList} edit={UserEdit} create={user.isAdmin ? UserCreate : null} />,
     ]}
   </Admin>
--- src/campus.js
+++ src/campus.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Create,Datagrid, TextField, EmailField, DateField, BooleanField, Edit, SimpleForm, TextInput, DateInput, BooleanInput, ReferenceInput, ReferenceField, SelectInput, Filter } from 'react-admin';
+import { List, Create, Datagrid, TextField, EmailField, DateField, BooleanField, Edit, SimpleForm, TextInput, DateInput, BooleanInput, ReferenceInput, ReferenceField, SelectInput, Filter } from 'react-admin';
 
 
 export const CampusList = props => (
@@ -12,8 +12,34 @@ export const CampusList = props => (
             <TextField source="address" />
             <TextField source="webUrl" />
             <TextField source="calendarUrl" />
-            <DateField source="createdAt" />
-            <DateField source="updatedAt" />
         </Datagrid>
     </List>
+);
+
+export const CampusEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+            <ReferenceInput source="dean.id" reference="user" label="Dean">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="address" />
+            <TextInput source="webUrl" />
+            <TextInput source="calendarUrl" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const CampusCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+            <ReferenceInput source="dean.id" reference="user" label="Dean">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="address" />
+            <TextInput source="webUrl" />
+            <TextInput source="calendarUrl" />
+        </SimpleForm>
+    </Create>
 );
