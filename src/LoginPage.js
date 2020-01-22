8ef6c25fac7072678e1f1cf7561cf45921d95229
Joe Reeve <joseph.reeve@googlemail.com>
2020-01-22 13:14:28 +0000

    :art: Auth Page Prettify

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useLogin, useNotify } from 'react-admin';
 import { postJSON } from './http';
 import config from './config';
-// import { ThemeProvider } from '@material-ui/styles';
 
 const LoginPage = ({ theme }) => {
     const [phone, setPhone] = useState('');
@@ -22,21 +21,44 @@ const LoginPage = ({ theme }) => {
             .catch(() => notify('Invalid code'));
     };
     return (
-        // <ThemeProvider theme={theme}>
-            // {
-                (stage === 'phone')
-                ?
-                <form onSubmit={submit}>
-                    <input name="phone" type="tel" value={phone} onChange={e => setPhone(e.target.value)} />
-                </form>
-                : <>
-                    <form onSubmit={submit}>
-                        <input name="code" type="text" value={code} onChange={e => setCode(e.target.value)} />
-                    </form>
-                    <button onClick={() => setStage('phone')}>No Code?</button>
-                </>
-            // }
-        // </ThemeProvider>
+        <div className='page'>
+            <div className='vertical-align'>
+                <div className='horizontal-align'>
+                    <div className='login-card'>
+                        <h1>Login</h1>
+                        <div>
+                            {
+                                (stage === 'phone')
+                                    ? <>
+                                        <form onSubmit={submit}>
+                                            <label for='phone'>Phone Number (+447XXXXXXXX)</label>
+                                            <br />
+                                            <br />
+                                            <input name="phone" id='phone' type="tel" value={phone} onChange={e => setPhone(e.target.value)} placeholder='+447XXXXXXXX' />
+                                            <br />
+                                            <br />
+                                            <button>Next</button>
+                                        </form>
+                                    </> : <>
+                                        <form onSubmit={submit}>
+                                            <label for='code'>Enter 6 digit code</label>
+                                            <br />
+                                            <br />
+                                            <input name="code" id='code' maxLength='6' type="text" value={code} onChange={e => setCode(e.target.value)} placeholder='X X X X X X' style={{textAlign:'center'}} />
+                                            <br />
+                                            <br />
+                                            <button>Login</button>
+                                        </form>
+                                        <br /><br />
+                                        <button onClick={() => setStage('phone')} className='no-code'>No Code?</button>
+                                    </>
+                            }
+                        </div>
+                        <div><br /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
